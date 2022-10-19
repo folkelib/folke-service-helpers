dfd1fb6a29d25428136bd9679a618c95753c60d9
Sidoine De Wispelaere <sidoine@sidoine.net>
2022-10-19 20:08:39 +0200

    chore: some package update

--- src/authorize/index.ts
+++ src/authorize/index.ts
@@ -1,3 +1,7 @@
 export * from "./api-authorization-routes";
 export * from "./authorize";
 export * from "./login-menu";
+export * from "./authorize-route";
+export * from "./redirect-or-render";
+export * from "./login";
+export * as Logout from "./logout";
